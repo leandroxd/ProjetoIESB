40676992a2fb498ffc1637380a7cf45b89d8fa9b
leandroxd <leandrocesargd_@hotmail.com>
2019-06-10 18:25:07 -0300

    Full functional Contact list

--- app/src/main/java/br/com/arturbc/projetoiesb/activities/ContactsActivity.java
+++ app/src/main/java/br/com/arturbc/projetoiesb/activities/ContactsActivity.java
@@ -29,6 +29,7 @@ import br.com.arturbc.projetoiesb.User;
 public class ContactsActivity extends AppCompatActivity {
 
     private GroupAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,7 +37,7 @@ public class ContactsActivity extends AppCompatActivity {
 
         RecyclerView rv = findViewById(R.id.recycler);
 
-        adapter = new GroupAdapter<>();
+        adapter = new GroupAdapter();
         rv.setAdapter(adapter);
         rv.setLayoutManager(new LinearLayoutManager(this));
 
@@ -56,22 +57,25 @@ public class ContactsActivity extends AppCompatActivity {
                         for(DocumentSnapshot doc : docs) {
                            User user = doc.toObject(User.class);
                             Log.d("DocumentSnapShot", user.getUsername());
+
                             adapter.add(new UserItem(user));
                         }
                     }
                 });
     }
     private class UserItem extends Item<ViewHolder> {
+
         private final User user;
 
         private UserItem(User user) {
+
             this.user = user;
         }
 
         @Override
         public void bind(@NonNull ViewHolder viewHolder, int position) {
             TextView txtUsername = viewHolder.itemView.findViewById(R.id.textView);
-            ImageView imgPhoto = viewHolder.itemView.findViewById(R.id.imageView);
+            ImageView imgPhoto = viewHolder.itemView.findViewById(R.id.imageView2);
 
             Picasso.get()
                     .load(user.getProfileUrl())
