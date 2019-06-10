2446c073a33ee82d00ce003224e6428abb1087e8
leandroxd <leandrocesargd_@hotmail.com>
2019-06-10 19:32:19 -0300

    Fixed some issues, chat populeted for test

--- app/src/main/java/br/com/arturbc/projetoiesb/activities/ChatActivity.java
+++ app/src/main/java/br/com/arturbc/projetoiesb/activities/ChatActivity.java
@@ -1,15 +1,59 @@
 package br.com.arturbc.projetoiesb.activities;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.xwray.groupie.GroupAdapter;
+import com.xwray.groupie.Item;
+import com.xwray.groupie.ViewHolder;
 
 import br.com.arturbc.projetoiesb.R;
 
 public class ChatActivity extends AppCompatActivity {
 
+    private GroupAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        RecyclerView rv = findViewById(R.id.recycle_chat);
+
+        adapter = new GroupAdapter();
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        rv.setAdapter(adapter);
+
+        //populando
+        adapter.add(new MessageItem(true));
+        adapter.add(new MessageItem(false));
+        adapter.add(new MessageItem(false));
+        adapter.add(new MessageItem(true));
+        adapter.add(new MessageItem(true));
+        adapter.add(new MessageItem(false));
+        adapter.add(new MessageItem(true));
+
+    }
+
+    private class MessageItem extends Item<ViewHolder>{
+
+        private final boolean isLeft;
+
+        private MessageItem(boolean isLeft) {
+            this.isLeft = isLeft;
+        }
+
+        @Override
+        public void bind(@NonNull ViewHolder viewHolder, int position) {
+
+        }
+
+        @Override
+        public int getLayout() {
+            return isLeft ? R.layout.item_from_message : R.layout.item_to_message;
+        }
     }
 }
