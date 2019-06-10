5931f3486a224a459adb55e32f9d415bf584e67c
leandroxd <leandrocesargd_@hotmail.com>
2019-06-10 18:57:23 -0300

    Chat intent start clicking on contacts (Just layout)

---
+++ app/src/main/java/br/com/arturbc/projetoiesb/activities/ChatActivity.java
@@ -0,0 +1,15 @@
+package br.com.arturbc.projetoiesb.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.arturbc.projetoiesb.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
